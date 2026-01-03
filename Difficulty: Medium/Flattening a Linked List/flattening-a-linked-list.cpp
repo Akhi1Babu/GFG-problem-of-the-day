32c55128a6cfdaa3ceab39074ede7b844d098138
Akhil babu <akhilb732@gmail.com>
2026-01-03 20:05:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Flattening a Linked List/flattening-a-linked-list.cpp
+++ Difficulty: Medium/Flattening a Linked List/flattening-a-linked-list.cpp
@@ -14,36 +14,46 @@ public:
 */
 
 class Solution {
+private:
+    Node* MergeList(Node*list1,Node*list2){
+        Node*resulthead=new Node(-1);
+        Node* resulttemp=resulthead;
+        while(list1!=NULL && list2!=NULL){
+            if(list1->data<=list2->data){
+                Node* newnode=new Node(list1->data);
+                list1=list1->bottom;
+                 resulttemp->bottom=newnode;
+            }
+            else{
+                
+                Node* newnode=new Node(list2->data);
+                list2=list2->bottom;
+                 resulttemp->bottom=newnode;
+                
+            }
+           
+            
+                resulttemp=resulttemp->bottom;
+                resulttemp->next=nullptr;
+  
+        }
+        if(list1){
+            resulttemp->bottom=list1;
+        }
+        else{
+            resulttemp->bottom=list2;
+        }
+        return resulthead->bottom;
+    }
   public:
     Node *flatten(Node *root) {
         // code here
-        vector<int> helper;
-       Node* currenthead=root;
-       Node* headtemp=root;
-   
-       if(root==NULL){
-           return NULL;
-       }
-      while (currenthead != NULL) { // Moves Right: 5 -> 10 -> 19 -> 28
-    Node* verticalTraveler = currenthead; 
-    
-    while (verticalTraveler != NULL) { // Moves Down each column
-        helper.push_back(verticalTraveler->data);
-        verticalTraveler = verticalTraveler->bottom;
-    }
-    
-    currenthead = currenthead->next; // Move to the next column safely
-}
-       sort(helper.begin(),helper.end());
-       int n=helper.size();
-       Node* resulthead=new Node(1);
-       Node* resulttemp=resulthead;
-       for(int i=0;i<n;i++){
-           Node* temp=new Node(helper[i]);
-           resulttemp->bottom=temp;
-           resulttemp=resulttemp->bottom;
-           
-       }
-       return resulthead->bottom;
+        if(root==NULL || root->next==NULL){
+            return root;
+        }
+        Node* root2=flatten(root->next);
+       return MergeList(root,root2);
+       
+        
     }
 };

40ea6850496912dbec6958f0a0868d4976e5b52b
Akhil babu <akhilb732@gmail.com>
2026-01-06 18:32:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Bottom View of Binary Tree/bottom-view-of-binary-tree.cpp
@@ -0,0 +1,45 @@
+/*
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int x) {
+        data = x;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    vector<int> bottomView(Node *root) {
+        // code here
+         map<int,int> mpp;
+        queue<pair<Node*,int>> que;
+        //doing level order Traversal...
+        que.push({root,0});
+        while(!que.empty()){
+            auto node=que.front();
+            que.pop();
+            int linenum=node.second;
+            //if(mpp.find(linenum)==mpp.end()){
+                mpp[linenum]=node.first->data;
+            //}
+            if(node.first->left){
+                que.push({node.first->left,linenum-1});
+            }
+            if(node.first->right){
+                que.push({node.first->right,linenum+1});
+            }
+
+        }
+        vector<int> result;
+        for(auto it :mpp){
+            result.push_back(it.second);
+        }
+        return result;
+        
+    }
+};
